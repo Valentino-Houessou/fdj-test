6588f62fdcd5021e2c5882e5e9df2795b295b618
Valentino-Houessou <valentino.houessou@gmail.com>
2021-01-19 04:01:48 +0100

    Structure PriceContainer

--- src/components/price/PriceContainer.tsx
+++ src/components/price/PriceContainer.tsx
@@ -17,33 +17,39 @@ export const PriceContainer: React.FC<PriceContainerProps> = ({ prices }) => {
   const [remainPattern, setRemainPattern] = useState([MIN_NUMBERS, MIN_STARS]);
 
   useEffect(() => {
+    // process the remaining number after the user choice
     const remainNumbers =
       MIN_NUMBERS > selectedPattern[0] ? MIN_NUMBERS - selectedPattern[0] : 0;
+    // process the remaining star after the user choice
     const remainStars =
       MIN_STARS > selectedPattern[1] ? MIN_STARS - selectedPattern[1] : 0;
+
+    //set the remainPattern
     setRemainPattern([remainNumbers, remainStars]);
   }, [selectedPattern]);
 
+  // compute the test for numbersCount
+  const numbersCount = remainPattern[0]
+    ? `${remainPattern[0]} ${remainPattern[0] > 1 ? " Numéros" : "Numéro"}`
+    : "";
+
+  // compute the test for "et"
+  const andDisplayer = remainPattern[0] && remainPattern[1] ? "et" : "";
+
+  // compute the test for starsCount
+  const starsCount = remainPattern[1]
+    ? `${remainPattern[1]} ${remainPattern[1] > 1 ? " Étoiles" : "Étoile"}`
+    : "";
+
+  const price = getPrice(selectedPattern, prices);
+
   return (
     <div className="priceContainer">
-      {remainPattern[0] > 0 || remainPattern[1] > 0
-        ? `choisissez ${
-            remainPattern[0]
-              ? `${remainPattern[0]} ${
-                  remainPattern[0] > 1 ? " Numéros" : "Numéro"
-                }`
-              : ""
-          } ${remainPattern[0] && remainPattern[1] ? "et" : ""} ${
-            remainPattern[1]
-              ? `${remainPattern[1]} ${
-                  remainPattern[1] > 1 ? " Étoiles" : "Étoile"
-                }`
-              : ""
-          }`
-        : `${getPrice(
-            selectedPattern,
-            prices
-          )}    Cochez d'autres numéros pour faire un multiple`}
+      {`Tarif: ${
+        remainPattern[0] > 0 || remainPattern[1] > 0
+          ? `choisissez ${numbersCount} ${andDisplayer} ${starsCount}`
+          : `${price}    Cochez d'autres numéros pour faire un multiple`
+      }`}
     </div>
   );
 };
